4713866c044e39703941da7070900b4d8d29efca
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-10 13:37:32 +0200

    Create YourVafouritesCard.js

---
+++ client/src/Components/YourFavourites/YourVafouritesCard.js
@@ -0,0 +1,15 @@
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+const YourFavouritesCard = ({ ...props }) => {
+  return (
+    <Grid item xs={3}>
+      <Paper elevation={3}>
+        <img className="movie-card-img" src={props.image.medium} alt="img" />
+      </Paper>
+    </Grid>
+  );
+};
+
+export default YourFavouritesCard;
