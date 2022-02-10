4afa9ad181fc614ccedef2583bdce6780675a83c
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-10 13:37:41 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { Route, Routes } from "react-router-dom";
+
+import Container from "@mui/material/Container";
+
+import Navbar from "./Components/Navbar/Navbar";
+import YourFavourites from "./Components/YourFavourites/YourFavourites";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<YourFavourites />} />
+      </Routes>
     </div>
   );
 }
