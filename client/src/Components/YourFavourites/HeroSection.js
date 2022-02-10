6b19703012b328a282d35fa9c00270baee91d479
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-10 23:57:28 +0200

    Update HeroSection.js

--- client/src/Components/YourFavourites/HeroSection.js
+++ client/src/Components/YourFavourites/HeroSection.js
@@ -9,12 +9,15 @@ const HeroSection = () => {
         alignItems: "flex-start",
         width: "100%",
         height: "300px",
+        backgroundColor: "#bfdbf7",
       }}
     >
       <Box
         style={{
           margin: "30px",
+          textAlign: "start",
         }}
+        className="hero-heading-box"
       >
         <Typography variant="h4" component="h1">
           Welcome to Cinema Fanatics
@@ -27,7 +30,8 @@ const HeroSection = () => {
           }}
         >
           Bingewatch all your favourite shows in one place. Explore from a vast
-          selection of shows, just 1 click away ! Find your favourite show now !
+          selection of comedies, drama, horror or sci-fi! Just one click away !
+          Find your favourite show now !
         </Typography>
         <Button
           variant="contained"
