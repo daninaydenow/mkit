1e237522b8fbfa16caea9fb559b5a676e1099a0f
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-10 23:57:32 +0200

    Update YourVafouritesCard.js

--- client/src/Components/YourFavourites/YourVafouritesCard.js
+++ client/src/Components/YourFavourites/YourVafouritesCard.js
@@ -1,13 +1,12 @@
 import { Grid, Card, CardActionArea, CardMedia } from "@mui/material";
 import { Link } from "react-router-dom";
-import api from "../../api";
 
 const YourFavouritesCard = ({ ...props }) => {
   return (
-    <Grid item xs={3} elevated={3}>
+    <Grid item xs={3}>
       <Card sx={{ maxHeight: "22.5rem" }}>
         <CardActionArea>
-          <Link to={`${api.baseUrl}/${props.id}`}>
+          <Link to={`/shows/${props.id}`}>
             <CardMedia
               component="img"
               height="100%"
