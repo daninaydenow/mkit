186c60ca99584a3370b1e35b125c4ef9e6b6978d
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-13 16:34:26 +0200

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    const conn = await mongoose.connect(
+      `mongodb+srv://naydenow123:naydenow123@naydenovcluster.xlxti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
