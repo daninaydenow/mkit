4940c74cfa7d064a7f665cec4499cdb979141866
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-13 16:34:34 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const connectDB = require("./config/db");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
+connectDB();
 const app = express();
 
 // view engine setup
