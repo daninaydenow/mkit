fdee014081db213fed0d07f83b0aa500f90f07bf
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-14 15:36:52 +0200

    Update authApi.js

--- client/src/endPoints/authApi.js
+++ client/src/endPoints/authApi.js
@@ -1,16 +1,1 @@
-const baseUrl = "http://localhost:8000/api/users";
-
-const login = async (data) => {
-  try {
-    const user = await fetch(`${baseUrl}/login`, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
-    });
-    return user;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export { login };
+export const authUrl = "http://localhost:8000/api/users";
