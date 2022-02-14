bd40242e3d422749c054998045276001db9e22a1
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-14 12:22:10 +0200

    Create favouriteController.js

---
+++ server/controllers/favouriteController.js
@@ -0,0 +1,57 @@
+const Favourite = require("../models/FavouriteModel");
+
+// @desc Get goals
+// @route GET api/goals
+// @access Private
+const getFavourites = async (req, res) => {
+  const favourites = await Favourite.find();
+  res.status(200).json(favourites);
+};
+
+// @desc Set goal
+// @route POST api/goals
+// @access Private
+const setFavourite = async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please adda text field");
+  }
+  const favourite = await Favourite.create({
+    text: req.body.text,
+  });
+  res.status(200).json(goal);
+};
+
+// @desc Update goal
+// @route Put api/goals/:id
+// @access Private
+const updateFavourite = async (req, res) => {
+  const favourite = await Favourite.findById(req.params.id);
+  if (!favourite) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await favourite.remove();
+  res.status(200).json({ id: req.params.id });
+};
+
+// @desc Delete goala
+// @route Delete api/goals/:id
+// @access Private
+const deleteFavourite = async (req, res) => {
+  const favourite = await Favourite.findById(req.params.id);
+  if (!favourite) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+  const deletedGoal = await Favourite.findByIdAndDelete(req.params.id);
+  res.status(200).json(deletedGoal);
+};
+
+module.exports = {
+  getFavourites,
+  setFavourite,
+  updateFavourite,
+  deleteFavourite,
+};
