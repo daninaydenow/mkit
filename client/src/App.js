660fd71a41f16b918b040e5b1545f263807d3e7f
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-14 12:21:13 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Navbar from "./Components/Navbar/Navbar";
 import YourFavourites from "./Components/YourFavourites/YourFavourites";
 import DetailsPage from "./Components/DetailsPage/DetailsPage";
 import SearchPage from "./Components/SearchPage/SearchPage";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/" element={<YourFavourites />} />
         <Route path="/shows/:showId" element={<DetailsPage />} />
         <Route path="/search" element={<SearchPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
