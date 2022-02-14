f2958e76a14c59493c330d109fa566c7b71c66d5
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-14 15:37:04 +0200

    Update Register.js

--- client/src/Components/Register/Register.js
+++ client/src/Components/Register/Register.js
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { validate } from "../../helpers/validateForm";
+import { useAuth } from "../../contexts/AuthContext";
 import {
   Container,
   FormControl,
@@ -8,53 +12,131 @@ import {
   Button,
 } from "@mui/material";
 
-import { Link } from "react-router-dom";
+const initialValues = { username: "", email: "", password: "", rePassword: "" };
 
 const Register = () => {
+  const navigate = useNavigate();
+  const { register } = useAuth();
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      const data = {
+        username: formValues.username,
+        email: formValues.email,
+        password: formValues.password,
+      };
+      register(data)
+        .then(() => {
+          navigate("/");
+        })
+        .catch((error) => {
+          if (error.code === "auth/email-already-in-use") {
+            setFormErrors({
+              ...formErrors,
+              email: "A user with the same email already exists!",
+            });
+          }
+        });
+    }
+  }, [formErrors, isSubmit, formValues, navigate, register]);
+
+  const registerHandler = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
   return (
-    <Container
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        minHeight: "100%",
-        width: "100%",
-        marginTop: "7rem",
-      }}
-    >
-      <Typography variant="h4" component="h1">
-        Register
-      </Typography>
-      <FormControl sx={{ width: "350px" }}>
-        <InputLabel htmlFor="username">Username</InputLabel>
-        <Input id="username" name="username" aria-describedby="username" />
-        <FormHelperText id="username">
-          The name you'll be recognized with ...
-        </FormHelperText>
-      </FormControl>
-      <FormControl sx={{ width: "350px" }}>
-        <InputLabel htmlFor="email">Email</InputLabel>
-        <Input id="email" name="email" aria-describedby="email" />
-        <FormHelperText id="email">
-          We'll never share your email.
-        </FormHelperText>
-      </FormControl>
-      <FormControl sx={{ width: "350px" }}>
-        <InputLabel htmlFor="password">Password</InputLabel>
-        <Input id="password" name="password" aria-describedby="password" />
-        <FormHelperText id="password">
-          We'll never share your password.
-        </FormHelperText>
-      </FormControl>
-      <Button sx={{ marginTop: "1rem" }}>Register</Button>
-      <Typography sx={{ marginTop: "1rem" }}>
-        Already have an account ?{" "}
-      </Typography>
-      <Link to="/login" style={{ marginTop: ".5rem" }}>
-        Login
-      </Link>
-    </Container>
+    <form method="POST" onSubmit={registerHandler}>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          minHeight: "100%",
+          width: "100%",
+          marginTop: "7rem",
+        }}
+      >
+        <Typography variant="h4" component="h1">
+          Register
+        </Typography>
+        <FormControl sx={{ width: "350px", margin: ".5rem" }}>
+          <InputLabel htmlFor="username">Username</InputLabel>
+          <Input
+            id="username"
+            name="username"
+            type="text"
+            aria-describedby="username"
+            value={formValues.username}
+            onChange={onChangeHandler}
+          />
+          <FormHelperText id="username">
+            The name you'll be recognized with ...
+          </FormHelperText>
+        </FormControl>
+        <FormControl sx={{ width: "350px", margin: ".5rem" }}>
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            aria-describedby="email"
+            value={formValues.email}
+            onChange={onChangeHandler}
+          />
+          <FormHelperText id="email">
+            We'll never share your email.
+          </FormHelperText>
+        </FormControl>
+        <FormControl sx={{ width: "350px", margin: ".5rem" }}>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            aria-describedby="password"
+            value={formValues.password}
+            onChange={onChangeHandler}
+          />
+          <FormHelperText id="password">
+            We'll never share your password.
+          </FormHelperText>
+        </FormControl>
+        <FormControl sx={{ width: "350px", margin: ".5rem" }}>
+          <InputLabel htmlFor="rePassword">Repeat Password</InputLabel>
+          <Input
+            id="rePassword"
+            name="rePassword"
+            type="password"
+            aria-describedby="rePassword"
+            value={formValues.rePassword}
+            onChange={onChangeHandler}
+          />
+          <FormHelperText id="rePassword">
+            We'll never share your password.
+          </FormHelperText>
+        </FormControl>
+        <Button type="submit" sx={{ marginTop: "1rem" }}>
+          Register
+        </Button>
+        <Typography sx={{ marginTop: "1rem" }}>
+          Already have an account ?{" "}
+        </Typography>
+        <Link to="/login" style={{ marginTop: ".5rem" }}>
+          Login
+        </Link>
+      </Container>
+    </form>
   );
 };
 
