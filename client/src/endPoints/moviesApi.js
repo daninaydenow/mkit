19ab5bf502a4d378b7c6eef778f62ba2a2ddb5ce
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-15 15:31:24 +0200

    Create moviesApi.js

---
+++ client/src/endPoints/moviesApi.js
@@ -0,0 +1,22 @@
+const baseUrl = "https://api.tvmaze.com";
+
+const getAll = async () => {
+  try {
+    return await fetch(`${baseUrl}/shows`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getOne = async (showId) => {
+  try {
+    return await fetch(`${baseUrl}/shows/${showId}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default {
+  getAll,
+  getOne,
+};
