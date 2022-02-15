8fa09dce25947904fe5fc1829973123a23e1e6e2
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-15 13:08:49 +0200

    Update JWT expiration timer

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -79,7 +79,7 @@ const getMe = asyncHandler(async (req, res) => {
 
 // Generate JWT
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30m" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
 module.exports = {
