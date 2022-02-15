f89db652eda776dee1821bfce95098e9d3475433
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-15 13:09:23 +0200

    Update favourites controller comments

--- server/controllers/favouriteController.js
+++ server/controllers/favouriteController.js
@@ -1,15 +1,15 @@
 const Favourite = require("../models/FavouriteModel");
 
-// @desc Get goals
-// @route GET api/goals
+// @desc Get favourite
+// @route GET api/favourite
 // @access Private
 const getFavourites = async (req, res) => {
-  const favourites = await Favourite.find();
+  const favourites = await Favourite.find({ user: req.user.id });
   res.status(200).json(favourites);
 };
 
-// @desc Set goal
-// @route POST api/goals
+// @desc Set favourite
+// @route POST api/favourite
 // @access Private
 const setFavourite = async (req, res) => {
   if (!req.body.text) {
@@ -17,13 +17,14 @@ const setFavourite = async (req, res) => {
     throw new Error("Please adda text field");
   }
   const favourite = await Favourite.create({
-    text: req.body.text,
+    movieId: req.body.text,
+    user: req.user.id,
   });
-  res.status(200).json(goal);
+  res.status(200).json(favourite);
 };
 
-// @desc Update goal
-// @route Put api/goals/:id
+// @desc Update favourite
+// @route Put api/favourite/:id
 // @access Private
 const updateFavourite = async (req, res) => {
   const favourite = await Favourite.findById(req.params.id);
@@ -36,8 +37,8 @@ const updateFavourite = async (req, res) => {
   res.status(200).json({ id: req.params.id });
 };
 
-// @desc Delete goala
-// @route Delete api/goals/:id
+// @desc Delete favourite
+// @route Delete api/favourite/:id
 // @access Private
 const deleteFavourite = async (req, res) => {
   const favourite = await Favourite.findById(req.params.id);
@@ -45,8 +46,8 @@ const deleteFavourite = async (req, res) => {
     res.status(400);
     throw new Error("Goal not found");
   }
-  const deletedGoal = await Favourite.findByIdAndDelete(req.params.id);
-  res.status(200).json(deletedGoal);
+  const deletedFavourite = await Favourite.findByIdAndDelete(req.params.id);
+  res.status(200).json(deletedFavourite);
 };
 
 module.exports = {
