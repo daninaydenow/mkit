06e1869cae006fb2f6a82dd0c50f4f912d655146
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-15 16:57:37 +0200

    fix wrong method

--- server/routes/favouriteRoutes.js
+++ server/routes/favouriteRoutes.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const {
   getFavourites,
   setFavourite,
-  updateFavourite,
   deleteFavourite,
 } = require("../controllers/favouriteController");
 
 const protect = require("../middleWare/authMiddleware");
 
 router.route("/").get(protect, getFavourites).post(protect, setFavourite);
-router.route("/:id").delete(protect, deleteFavourite).put(updateFavourite);
+router.route("/:id").post(protect, deleteFavourite);
 
 module.exports = router;
