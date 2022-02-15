a09156b4eebabb87356793cc2ec93a94d612ceb1
Daniel Naydenov <daninaydenow@gmail.com>
2022-02-15 15:29:33 +0200

    Update required variables

--- server/controllers/favouriteController.js
+++ server/controllers/favouriteController.js
@@ -1,52 +1,41 @@
 const Favourite = require("../models/FavouriteModel");
+const asyncHandler = require("express-async-handler");
 
 // @desc Get favourite
 // @route GET api/favourite
 // @access Private
-const getFavourites = async (req, res) => {
+const getFavourites = asyncHandler(async (req, res) => {
   const favourites = await Favourite.find({ user: req.user.id });
   res.status(200).json(favourites);
-};
+});
 
 // @desc Set favourite
 // @route POST api/favourite
 // @access Private
-const setFavourite = async (req, res) => {
-  if (!req.body.text) {
+const setFavourite = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  if (!req.body.movieId) {
     res.status(400);
-    throw new Error("Please adda text field");
+    throw new Error("Please add movie Id");
   }
+  console.log(req.body);
   const favourite = await Favourite.create({
-    movieId: req.body.text,
+    movieId: req.body.movieId,
     user: req.user.id,
   });
   res.status(200).json(favourite);
-};
-
-// @desc Update favourite
-// @route Put api/favourite/:id
-// @access Private
-const updateFavourite = async (req, res) => {
-  const favourite = await Favourite.findById(req.params.id);
-  if (!favourite) {
-    res.status(400);
-    throw new Error("Goal not found");
-  }
-
-  await favourite.remove();
-  res.status(200).json({ id: req.params.id });
-};
+});
 
 // @desc Delete favourite
 // @route Delete api/favourite/:id
 // @access Private
 const deleteFavourite = async (req, res) => {
-  const favourite = await Favourite.findById(req.params.id);
+  const favourite = await Favourite.findById(req.body.movieId);
   if (!favourite) {
     res.status(400);
-    throw new Error("Goal not found");
+    throw new Error("Favourite not found");
   }
-  const deletedFavourite = await Favourite.findByIdAndDelete(req.params.id);
+  const deletedFavourite = await Favourite.findByIdAndDelete(req.body.movieId);
   res.status(200).json(deletedFavourite);
 };
 
